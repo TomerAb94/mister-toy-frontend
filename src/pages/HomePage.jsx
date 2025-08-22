66d6a14e87f8ddfc94e6495f8818b8d8547498b9
Tomer1994Ab <Abarbanel1994@gmail.com>
2025-08-22 17:43:19 +0300

    Init toy frontend app

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export function HomePage() {
+    return (
+        <section className="home-page">
+            <h1>Welcome to Mister Toy!</h1>
+            <img src="https://res.cloudinary.com/dzcmadjlq/image/upload/v1696115246/mister-toy-home_ozkqzj.jpg" alt="Mister Toy" />
+        </section>
+    )
+}
