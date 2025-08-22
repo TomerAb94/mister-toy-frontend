9465e6c09d3f6b2107a72ac2a50e09dc63eccc8c
Tomer1994Ab <Abarbanel1994@gmail.com>
2025-08-22 18:04:00 +0300

    Add basic pages and cmps. no CRUD yet

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import { AppHeader } from './cmps/AppHeader.jsx'
-// import { AppFooter } from './cmps/AppFooter.jsx'
+import { AppFooter } from './cmps/AppFooter.jsx'
 
-// import { HomePage } from './pages/HomePage.jsx'
-// import { AboutUs } from './pages/AboutUs.jsx'
-// import { ToyIndex } from './pages/ToyIndex.jsx'
+import { HomePage } from './pages/HomePage.jsx'
+import { AboutUs } from './pages/AboutUs.jsx'
+import { ToyIndex } from './pages/ToyIndex.jsx'
 import { store } from './store/store.js'
 // import { ToyEdit } from './pages/ToyEdit.jsx'
 // import { ToyDetails } from './pages/ToyDetails.jsx'
@@ -21,16 +21,16 @@ export default function App() {
           <AppHeader />
           <main className="main-layout">
             <Routes>
-              {/* <Route element={<HomePage />} path="/" /> */}
-              {/* <Route element={<AboutUs />} path="/about" /> */}
-              {/* <Route element={<ToyIndex />} path="/toy" /> */}
+              <Route element={<HomePage />} path="/" />
+              <Route element={<AboutUs />} path="/about" />
+              <Route element={<ToyIndex />} path="/toy" />
               {/* <Route element={<ToyEdit />} path="/toy/edit" /> */}
               {/* <Route element={<ToyEdit />} path="/toy/edit/:toyId" /> */}
               {/* <Route element={<ToyDetails />} path="/toy/:toyId" /> */}
               {/* <Route element={<UserDetails />} path="/user/:userId" /> */}
             </Routes>
           </main>
-          {/* <AppFooter /> */}
+          <AppFooter />
         </section>
       </Router>
     </Provider>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,36 @@
+import logoImg from "../assets/img/logo.jpg"
+
+// import { useDispatch, useSelector } from "react-redux"
+// import { CHANGE_BY } from "../store/reducers/user.reducer.js"
+// import { useState } from "react"
+
+// const { useState } = React
+// const { useSelector, useDispatch } = ReactRedux
+
+
 export function HomePage() {
+    // const dispatch = useDispatch()
+    // const [_count, setCount] = useState(10)
+    // const count = useSelector(storeState => storeState.count)
+
+    // function changeCount(diff) {
+    //     // setCount(count => count + diff)
+    //     // dispatch({ type: INCREMENT })
+    //     dispatch({ type: CHANGE_BY, diff })
+    // }
+
     return (
-        <section className="home-page">
-            <h1>Welcome to Mister Toy!</h1>
-            <img src="https://res.cloudinary.com/dzcmadjlq/image/upload/v1696115246/mister-toy-home_ozkqzj.jpg" alt="Mister Toy" />
-        </section>
+        <section>
+            {/* <h2> */}
+                {/* Count {count} */}
+                {/* <button onClick={() => { */}
+                    {/* changeCount(1) */}
+                {/* }}>+</button> */}
+                {/* <button onClick={() => { */}
+                    {/* changeCount(10) */}
+                {/* }}>+10</button> */}
+            {/* </h2 > */}
+            <img src={logoImg} />
+        </section >
     )
 }
